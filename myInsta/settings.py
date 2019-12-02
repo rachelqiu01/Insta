ed02044893faf47b3dd77f00db6e455a34b429e8
rachelqiu01 <qiuyuan1992@gmail.com>
2019-12-01 18:48:03 -0800

    deploy

--- myInsta/settings.py
+++ myInsta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zewfk#^^(q-o66p%^9p$l0oa=h_5gv&9#&jf^-4a^aq-dxg2!x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'Ins',
     'imagekit',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
