6d7f2a1b13d0e3e94e2280c82d8c87604bb8ae36
rachelqiu01 <qiuyuan1992@gmail.com>
2019-12-01 18:02:32 -0800

    Finish temp website

--- Ins/admin.py
+++ Ins/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from Ins.models import Post, PostTwo, InsUser, Like, UserConnection
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(InsUser)
+admin.site.register(Like)
+admin.site.register(UserConnection)
--- Ins/urls.py
+++ Ins/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from Ins.views import HelloWorld
+from Ins.views import (HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, SignUp, addLike, UserDetailView)
+
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='home')
+    path('helloworld/', HelloWorld.as_view(), name='home'),
+    path('', PostsView.as_view(), name = 'posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
+    path('user/<int:pk>', UserDetailView.as_view(), name = 'user_detail'),
+    path('post/new', PostCreateView.as_view(), name = 'make_post'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name = 'post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name = 'post_delete'),
+    path('like', addLike, name='addLike'),
+    
 ]
